98b3892dcfdbc39ca37243943e9d0aea0624213b
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-04-22 23:01:42 +0900

    Import

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+#![feature(libc)]
+extern crate libc;
+
+#[link(name = "groonga")]
+extern {
+    pub fn grn_init() -> u64;
+    pub fn grn_ctx_open(rc: i32) -> u64;
+    pub fn grn_ctx_close(ctx: *mut libc::c_void) -> u64;
+    pub fn grn_db_open(ctx: *mut libc::c_void, dbpath: *mut libc::c_char) -> *mut libc::c_void;
+    pub fn grn_db_create(ctx: *mut libc::c_void, dbpath: *mut libc::c_char,
+                         flag: Option<extern "C" fn(libc::c_int) -> libc::c_int>) -> *mut libc::c_void;
+    pub fn grn_ctx_send(ctx: *mut libc::c_void, command: libc::c_char, command_length: libc::c_int,
+                        flag: libc::c_int) -> u64;
+    pub fn grn_ctx_recv(ctx: *mut libc::c_void, command: *mut libc::c_char, command_length: *mut libc::c_int,
+                        flag: *mut libc::c_int) -> u64;
+    pub fn grn_get_version() -> libc::c_char;
+    pub fn grn_fin() -> u64;
+}
