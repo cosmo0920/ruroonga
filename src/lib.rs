53206ac7078ae277f7caa1273d8dec9ce44f15d4
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-04-23 00:01:06 +0900

    Fix return types

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ extern crate libc;
 #[link(name = "groonga")]
 extern {
     pub fn grn_init() -> u64;
-    pub fn grn_ctx_open(rc: i32) -> u64;
+    pub fn grn_ctx_open(rc: u64) -> *mut libc::c_void;
     pub fn grn_ctx_close(ctx: *mut libc::c_void) -> u64;
     pub fn grn_db_open(ctx: *mut libc::c_void, dbpath: *mut libc::c_char) -> *mut libc::c_void;
     pub fn grn_db_create(ctx: *mut libc::c_void, dbpath: *mut libc::c_char,
@@ -13,6 +13,6 @@ extern {
                         flag: libc::c_int) -> u64;
     pub fn grn_ctx_recv(ctx: *mut libc::c_void, command: *mut libc::c_char, command_length: *mut libc::c_int,
                         flag: *mut libc::c_int) -> u64;
-    pub fn grn_get_version() -> libc::c_char;
+    pub fn grn_get_version() -> *mut libc::c_char;
     pub fn grn_fin() -> u64;
 }
