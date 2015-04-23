38e1f8a9e438d5621b59bf36d9d716e7537e3b89
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-04-24 08:50:34 +0900

    Convert Rust string type to C char type with unstable feature.... :<

--- examples/main.rs
+++ examples/main.rs
@@ -1,11 +1,23 @@
+#![feature(libc)]
+#![feature(collections)]
+#![feature(convert)]
 extern crate ruroonga;
+extern crate libc;
 use ruroonga::*;
 use std::ffi::CStr;
+use std::ffi::CString;
 use std::str;
+use std::string::String;
 
 fn main() {
     unsafe {
         let ctx = groonga_init();
+        let mut string = "test.db".to_string();
+        let bytes = string.into_bytes();
+        let mut x : Vec<libc::c_char> = bytes.map_in_place(|w| w as libc::c_char);;
+        let slice = x.as_mut_slice();
+        let dbpath = slice.as_mut_ptr();
+        let db_ctx = grn_db_create(ctx, dbpath, None);
         println!("Hello in Ruroonga with Groonga: {}", get_groonga_version());
         let _ = groonga_fin(ctx);
     }
