860b20a7e4ff6a2b16e2a7e38ea5b4c55844cfd8
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-04-25 00:10:21 +0900

    Skip creating db if it exists

--- examples/main.rs
+++ examples/main.rs
@@ -1,6 +1,7 @@
 #![feature(libc)]
 #![feature(collections)]
 #![feature(convert)]
+#![feature(path_ext)]
 extern crate ruroonga;
 extern crate libc;
 use ruroonga::*;
@@ -8,13 +9,22 @@ use std::ffi::CStr;
 use std::ffi::CString;
 use std::str;
 use std::string::String;
+use std::path::Path;
+use std::fs::PathExt;
 
 fn main() {
     unsafe {
         let ctx = groonga_init();
         let dbpath = "test.db";
         let c_dbpath = convert_str_to_cstr(dbpath);
-        let db_ctx = grn_db_create(ctx, c_dbpath, None);
+        let path = Path::new(dbpath);
+        let path_displayable = path.display();
+        let db_ctx = if path.exists() {
+            println!("{} exists create db skipped.", path_displayable);
+            grn_db_open(ctx, c_dbpath);
+        } else {
+            grn_db_create(ctx, c_dbpath, None);
+        };
         println!("Hello in Ruroonga with Groonga: {}", get_groonga_version());
         let _ = groonga_fin(ctx);
     }
