4a6a1df1e798176d718ca7994d549778dbeeace3
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-04-26 19:22:17 +0900

    Re-enable util functions

--- src/lib.rs
+++ src/lib.rs
@@ -40,23 +40,24 @@ pub fn groonga_fin(ctx: *mut groonga::grn_ctx) {
 //     }
 // }
 
-// pub fn get_groonga_version() -> &'static str {
-//     unsafe {
-//         return convert_cstr_to_str(groonga::grn_get_version());
-//     }
-// }
+pub fn get_groonga_version() -> &'static str {
+    unsafe {
+        let slice = CStr::from_ptr(groonga::grn_get_version());
+        return str::from_utf8(slice.to_bytes()).unwrap();
+    }
+}
 
-// pub fn convert_cstr_to_str(cstr: *mut libc::c_char) -> &'static str {
-//     unsafe {
-//         let slice = CStr::from_ptr(cstr);
-//         return str::from_utf8(slice.to_bytes()).unwrap();
-//     }
-// }
+pub fn convert_cstr_to_str(cstr: *mut libc::c_char) -> &'static str {
+    unsafe {
+        let slice = CStr::from_ptr(cstr);
+        return str::from_utf8(slice.to_bytes()).unwrap();
+    }
+}
 
-// pub fn convert_str_to_cstr(s: &str) -> *mut libc::c_char {
-//     let string = s.to_string();
-//     let bytes = string.into_bytes();
-//     let mut x : Vec<libc::c_char> = bytes.map_in_place(|w| w as libc::c_char);;
-//     let slice = x.as_mut_slice();
-//     return slice.as_mut_ptr();
-// }
+pub fn convert_str_to_cstr(s: &str) -> *mut libc::c_char {
+    let string = s.to_string();
+    let bytes = string.into_bytes();
+    let mut x : Vec<libc::c_char> = bytes.map_in_place(|w| w as libc::c_char);;
+    let slice = x.as_mut_slice();
+    return slice.as_mut_ptr();
+}
