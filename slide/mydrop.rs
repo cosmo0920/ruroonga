3e64c5dcc8ef3b2db1f57c8e203cf4144ae40a81
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-09-16 00:17:08 +0900

    slide: Add slide

---
+++ slide/mydrop.rs
@@ -0,0 +1,14 @@
+/// rustc mydrop.rs
+/// ./mydrop
+struct MyDrop;
+
+impl Drop for MyDrop {
+    fn drop(&mut self) {
+        println!("Dropping!");
+    }
+}
+
+fn main() {
+    let _x = MyDrop;
+}
+/// #=> Dropping!
